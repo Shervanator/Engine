c5fb0a1677cbf9253bee8cefd04e00a89a9b61bd
Shervin Aflatooni <shervinaflatooni@gmail.com>
2016-07-27 22:14:02 +1000

    try and pass ndk dir to cmake

--- scripts/cmake-android.sh
+++ scripts/cmake-android.sh
@@ -6,7 +6,7 @@ BUILD_TYPE=${BUILD_TYPE:-Release}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin-android
 cd $DIR/../bin-android
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DANDROID_NATIVE_API_LEVEL=17 -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 -DCMAKE_TOOLCHAIN_FILE=$DIR/../cmake/toolchains/android.toolchain.cmake
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DANDROID_NDK=$ANDROID_NDK -DANDROID_NATIVE_API_LEVEL=17 -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 -DCMAKE_TOOLCHAIN_FILE=$DIR/../cmake/toolchains/android.toolchain.cmake
 make $*
 make android-build
 
