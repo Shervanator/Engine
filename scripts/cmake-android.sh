817b99846c709c9aca0cfbc024fdbaba361e9450
Shervin Aflatooni <shervinaflatooni@gmail.com>
2016-07-30 17:20:31 +1000

    Use built in android SDK in travis remove ANDROID_SDK variable

--- scripts/cmake-android.sh
+++ scripts/cmake-android.sh
@@ -6,11 +6,11 @@ BUILD_TYPE=${BUILD_TYPE:-Release}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin-android
 cd $DIR/../bin-android
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DANDROID_NDK=$ANDROID_NDK -DANDROID_NATIVE_API_LEVEL=17 -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 -DCMAKE_TOOLCHAIN_FILE=$DIR/../cmake/toolchains/android.toolchain.cmake
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DANDROID_NDK=$ANDROID_NDK -DANDROID_NATIVE_API_LEVEL=22 -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 -DCMAKE_TOOLCHAIN_FILE=$DIR/../cmake/toolchains/android.toolchain.cmake
 make $*
 make android-build
 
-STATE=$(${ANDROID_SDK}/platform-tools/adb get-state || exit 0)
+STATE=$(adb get-state || exit 0)
 if [ "$STATE" == "device" ]; then
 	make android-install android-start
 fi
