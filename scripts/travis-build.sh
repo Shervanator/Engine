b5e0f85a0a56a768c347cde35cffb700c84e3993
Shervin Aflatooni <shervinaflatooni@gmail.com>
2016-07-30 18:25:26 +1000

    try installing gcc as well

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -4,7 +4,5 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cmake --version
-echo $ANDROID_NDK
-ANDROID_NDK=$ANDROID_NDK $DIR/cmake-android.sh -j4
 $DIR/cmake-make.sh -j4
+ANDROID_NDK=$ANDROID_NDK $DIR/cmake-android.sh -j4
