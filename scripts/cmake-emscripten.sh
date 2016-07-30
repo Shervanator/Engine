420fa743a21cb48308ef8b71fce5ba7dfb103fd9
Shervin Aflatooni <shervinaflatooni@gmail.com>
2016-07-31 01:55:41 +1000

    Add emscripten build

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-EMSCRIPTEN_PATH=${EMSCRIPTEN_PATH:-/usr/local/opt/emscripten/libexec}
+EMSCRIPTEN=${EMSCRIPTEN:-/usr/local/opt/emscripten/libexec}
 BUILD_TYPE=${BUILD_TYPE:-Release}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin-emscripten
 mkdir -p $DIR/../bin-emscripten/bin/
 cp $DIR/../src/html/index.html $DIR/../bin-emscripten/bin/
 cd $DIR/../bin-emscripten
-cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN_PATH/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN_PATH/Modules/cmake ../
+cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN/Modules/cmake ../
 make $*
--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -8,4 +8,6 @@ if [ $BUILD_TYPE == android ]; then
   ANDROID_NDK=$ANDROID_NDK $DIR/cmake-android.sh -j4
 elif [ $BUILD_TYPE == native ]; then
   $DIR/cmake-make.sh -j4
+elif [ $BUILD_TYPE == emscripten ]; then
+  $DIR/cmake-emscripten.sh -j4
 fi
