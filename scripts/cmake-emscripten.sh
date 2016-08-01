17bfecd6c91828b3289b6616970cd8cdad0e3fd8
Shervin Aflatooni <shervinaflatooni@gmail.com>
2016-08-01 21:41:13 +1000

    Move index.html to example folder

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -7,7 +7,7 @@ BUILD_TYPE=${BUILD_TYPE:-Release}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin-emscripten
 mkdir -p $DIR/../bin-emscripten/bin/
-cp $DIR/../src/html/index.html $DIR/../bin-emscripten/bin/
+cp $DIR/../src/example/html/index.html $DIR/../bin-emscripten/bin/
 cd $DIR/../bin-emscripten
 cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN/Modules/cmake ../
 make $*
