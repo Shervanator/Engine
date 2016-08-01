188f2b0fe9b768fb17071381964c9cdc8a03fc4e
Shervin Aflatooni <shervinaflatooni@gmail.com>
2016-08-01 16:59:34 +1000

    Move Xcode script to scripts folder

--- cmake-xcode.sh
+++
@@ -1,7 +0,0 @@
-BUILD_TYPE=Debug
-
-rm -rf bin-xcode
-mkdir bin-xcode
-cd bin-xcode
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G Xcode ../src/
-ln -s ../assets ./assets
---
+++ scripts/cmake-xcode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+BUILD_TYPE=${BUILD_TYPE:-Debug}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p $DIR/../bin
+cd $DIR/../bin
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -G Xcode
