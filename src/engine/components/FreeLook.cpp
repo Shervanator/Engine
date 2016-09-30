5926bfeb66fc8416c5d9fd11a2328d26a4a7380b
Shervin Aflatooni <shervinaflatooni@gmail.com>
2016-10-01 02:31:02 +1000

    Use proper struct syntax

--- src/engine/components/FreeLook.cpp
+++ src/engine/components/FreeLook.cpp
@@ -12,7 +12,7 @@ FreeLook::FreeLook(float speed)
   horizontal_r=0;
   vertical_r=0;
 
-  m_properties["speed"] = Property{FLOAT, &m_speed, 0, 0.01};
+  m_properties["speed"] = (Property){FLOAT, &m_speed, 0, 0.01};
 }
 
 FreeLook::~FreeLook(void)
--- src/engine/components/OrthoCamera.cpp
+++ src/engine/components/OrthoCamera.cpp
@@ -14,10 +14,10 @@ OrthoCamera::OrthoCamera(float aspect, float scale, float zNear, float zFar)
   m_zNear = zNear;
   m_zFar = zFar;
 
-  m_properties["scale"] = Property{FLOAT, &m_scale, 0, 200};
-  m_properties["aspect"] = Property{FLOAT, &m_aspect, 0, 10};
-  m_properties["zNear"] = Property{FLOAT, &m_zNear, 0, 1};
-  m_properties["zFar"] = Property{FLOAT, &m_zFar, 0, 1000};
+  m_properties["scale"] = (Property){FLOAT, &m_scale, 0, 200};
+  m_properties["aspect"] = (Property){FLOAT, &m_aspect, 0, 10};
+  m_properties["zNear"] = (Property){FLOAT, &m_zNear, 0, 1};
+  m_properties["zFar"] = (Property){FLOAT, &m_zFar, 0, 1000};
 }
 
 glm::mat4 OrthoCamera::getProjectionMatrix(void) const
