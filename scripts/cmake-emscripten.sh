bacd10aa297e8c7b65259ceb98cfcc609a381d4c
Shervin Aflatooni <shervinaflatooni@gmail.com>
2018-02-19 02:07:20 +1100

    fix folder reference

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -6,7 +6,7 @@ BUILD_TYPE=${BUILD_TYPE:-Release}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin-emscripten
 mkdir -p $DIR/../bin-emscripten/bin/
-cp $DIR/../Engine/example/html/index.html $DIR/../bin-emscripten/bin/
+cp $DIR/../src/example/html/index.html $DIR/../bin-emscripten/bin/
 cd $DIR/../bin-emscripten
 cmake -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
 make $*
