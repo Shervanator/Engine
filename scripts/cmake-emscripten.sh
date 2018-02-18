287ed4efe956c003b8e9ea867d9390982cedf30b
Shervin Aflatooni <shervinaflatooni@gmail.com>
2018-02-18 23:15:36 +1100

    find emscripten path

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -9,5 +9,6 @@ mkdir -p $DIR/../bin-emscripten
 mkdir -p $DIR/../bin-emscripten/bin/
 cp $DIR/../src/example/html/index.html $DIR/../bin-emscripten/bin/
 cd $DIR/../bin-emscripten
-cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/**/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN/**/cmake/Modules/ ../
+EMSCRIPTEN_FOLDER="$( echo $EMSCRIPTEN/emscripten/**/ )"
+cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN_FOLDER/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN_FOLDER/cmake/Modules/ ../
 make $*
