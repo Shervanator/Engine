8fa296dd527caf6d64be04cbe3e473d1d672f600
Shervin Aflatooni <shervinaflatooni@gmail.com>
2018-02-18 23:34:35 +1100

    add some debugging

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -10,5 +10,8 @@ mkdir -p $DIR/../bin-emscripten/bin/
 cp $DIR/../src/example/html/index.html $DIR/../bin-emscripten/bin/
 cd $DIR/../bin-emscripten
 EMSCRIPTEN_FOLDER="$( echo $EMSCRIPTEN/emscripten/**/ )"
+echo "RUNNING /home/travis/build/Shervanator/Engine/emsdk-portable/emscripten/1.37.34/emcc"
+/home/travis/build/Shervanator/Engine/emsdk-portable/emscripten/1.37.34/emcc
+echo "DONE"
 cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN_FOLDER/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN_FOLDER/cmake/Modules/ ../
 make $*
