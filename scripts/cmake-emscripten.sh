f93318a1dc343926defdc8f54b2855662c021824
Shervin Aflatooni <shervinaflatooni@gmail.com>
2018-02-19 02:03:03 +1100

    try and start travis setup from scratch

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -2,18 +2,11 @@
 
 set -e
 
-EMSCRIPTEN=${EMSCRIPTEN:-/usr/local/opt/emscripten/libexec}
 BUILD_TYPE=${BUILD_TYPE:-Release}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin-emscripten
 mkdir -p $DIR/../bin-emscripten/bin/
-cp $DIR/../src/example/html/index.html $DIR/../bin-emscripten/bin/
+cp $DIR/../Engine/example/html/index.html $DIR/../bin-emscripten/bin/
 cd $DIR/../bin-emscripten
-EMSCRIPTEN_FOLDER="$( echo $EMSCRIPTEN/emscripten/**/ )"
-echo "LOL"
-/home/travis/build/Shervanator/Engine/emsdk-portable/clang/e1.37.34_64bit/clang++ -v
-echo "RUNNING /home/travis/build/Shervanator/Engine/emsdk-portable/emscripten/1.37.34/emcc"
-/home/travis/build/Shervanator/Engine/emsdk-portable/emscripten/1.37.34/emcc
-echo "DONE"
-cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN_FOLDER/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN_FOLDER/cmake/Modules/ ../
+cmake -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
 make $*
--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -4,10 +4,10 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ $BUILD_TYPE == android ]; then
+if [[ $BUILD_TYPE == android ]]; then
   ANDROID_NDK=$ANDROID_NDK $DIR/cmake-android.sh -j4
-elif [ $BUILD_TYPE == native ]; then
+elif [[ $BUILD_TYPE == native ]]; then
   $DIR/cmake-make.sh -j4
-elif [ $BUILD_TYPE == emscripten ]; then
-  EMSCRIPTEN=$EMSDK $DIR/cmake-emscripten.sh -j4
+elif [[ $BUILD_TYPE == emscripten ]] ; then
+  $DIR/cmake-emscripten.sh -j4
 fi
